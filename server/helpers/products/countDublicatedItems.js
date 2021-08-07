0cb0b651cc5eede4f9f23adfb3df4225e20de16b
swaperX <mesut.nedzhib@gmail.com>
2021-08-07 13:31:06 +0300

    commit

---
+++ server/helpers/products/countDublicatedItems.js
@@ -0,0 +1,23 @@
+const countDublicatedItems = (data) => {
+  let newArray = [];
+  let counts = {};
+
+  for (let i = 0; i < data?.length; i++) {
+    if (counts[data[i]]) {
+      counts[data[i]] += 1;
+    } else {
+      counts[data[i]] = 1;
+    }
+  }
+
+  for (let prop in counts) {
+    newArray.push({
+      name: prop,
+      count: counts[prop],
+      // isChecked: false,
+    });
+  }
+  return newArray;
+};
+
+module.exports = countDublicatedItems;
