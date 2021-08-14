228f2c92fe511bbcea5cbe649aa3d6c079053ac0
swaperX <mesut.nedzhib@gmail.com>
2021-08-14 12:02:45 +0300

    commit

--- client/src/index.js
+++ client/src/index.js
@@ -9,11 +9,11 @@ import store from "./store";
 
 ReactDOM.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>
+    {/* <React.StrictMode> */}
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+    {/* </React.StrictMode> */}
   </Provider>,
   document.getElementById("root")
 );
--- client/src/screens/ProductDetailsScreen/ProductDetailsScreen.js
+++ client/src/screens/ProductDetailsScreen/ProductDetailsScreen.js
@@ -55,194 +55,194 @@ function ProductDetailsScreen() {
   };
   return (
     <div className="productDetailsScreen">
-      {loading ? (
+      {/* {loading ? (
         <LoadingBox />
       ) : error ? (
         <MessageBox message={error} variant={"error"} />
-      ) : (
-        <div className="productDetailsScreen-container">
-          <div className="productDetailsScreen-header">
-            <span>{product?.data?.name}</span>
-          </div>
-          <div className="productDetailsScreen-body">
-            <div className="productDetailsScreen-body-basic-info">
-              <>
-                <Carousel
-                  id="carousel"
-                  showStatus={false}
-                  showArrows={false}
-                  showIndicators={false}
-                  emulateTouch
-                  infiniteLoop
-                  autoPlay={true}
-                  showThumbs={false}
-                >
-                  {product?.data?.images?.map((item, index) => (
-                    <img key={index} src={item} alt={item} />
-                  ))}
-                </Carousel>
-
-                <Carousel
-                  showStatus={false}
-                  showArrows={false}
-                  showIndicators={false}
-                  emulateTouch
-                  infiniteLoop
-                  autoPlay={true}
-                  showThumbs={false}
-                  interval={5000}
-                >
-                  {images.map((item, index) => (
-                    <img key={index} src={item} alt={item} />
-                  ))}
-                </Carousel>
-
-                <Carousel
-                  id="carousel"
-                  showStatus={false}
-                  showArrows={false}
-                  showIndicators={false}
-                  emulateTouch
-                  infiniteLoop
-                  autoPlay={true}
-                  showThumbs={false}
-                >
-                  {product?.data?.images?.map((item, index) => (
-                    <img key={index} src={item} alt={item} />
-                  ))}
-                </Carousel>
-              </>
-            </div>
-            <div className="productDetailsScreen-body-carousel">
+      ) : ( */}
+      <div className="productDetailsScreen-container">
+        <div className="productDetailsScreen-header">
+          <span>{product?.data?.name}</span>
+        </div>
+        <div className="productDetailsScreen-body">
+          <div className="productDetailsScreen-body-basic-info">
+            <>
               <Carousel
                 id="carousel"
                 showStatus={false}
-                showArrows={true}
-                // showIndicators={false}
+                showArrows={false}
+                showIndicators={false}
                 emulateTouch
-                // infiniteLoop
-                autoPlay={false}
-                // showThumbs={false}
-                thumbWidth={100}
+                infiniteLoop
+                autoPlay={true}
+                showThumbs={false}
               >
                 {product?.data?.images?.map((item, index) => (
                   <img key={index} src={item} alt={item} />
                 ))}
               </Carousel>
-            </div>
-            <div className="productDetailsScreen-body-basic-info">
-              <div className="price">${product?.data?.price}</div>
-              <div>
-                <h3>Characteristics</h3>
-                <ul>
-                  <li>
-                    <CheckSharpIcon className="icon" />{" "}
-                    <span>{product?.data?.processorModel}</span>
-                  </li>
-                  <li>
-                    <CheckSharpIcon className="icon" />{" "}
-                    <span>{product?.data?.memory}</span>
-                  </li>
-                  <li>
-                    <CheckSharpIcon className="icon" />{" "}
-                    <span>{product?.data?.video}</span>
-                  </li>
-                  <li>
-                    <CheckSharpIcon className="icon" />{" "}
-                    <span>{product?.data?.storage}</span>
-                  </li>
-                </ul>
-              </div>
 
-              <div className="available">
-                {product?.data?.countInStock === 0 ? (
-                  ""
-                ) : (
-                  <>
-                    <CheckCircleIcon />
-                    <span>Available</span>
-                  </>
-                )}
-              </div>
-              <div className="qunatity">
-                {/* <RemoveIcon onClick={() => currentQuantityMinusHandle()} /> */}
-                <input
-                  type="number"
-                  min="1"
-                  defaultValue="1"
-                  onChange={(e) => setCurrentQuantity(e.target.value)}
-                />
-                {/* <AddIcon onClick={() => currentQuantityPlusHandle()} /> */}
-              </div>
-              <div
-                className="buyBtn"
-                onClick={() =>
-                  addToCartHandle({
-                    _id: product.data?._id,
-                    name: product?.data?.name,
-                    image: product?.data?.images[0],
-                    processorModel: product?.data?.processorModel,
-                    memory: product?.data?.memory,
-                    video: product?.data?.video,
-                    storage: product?.data?.storage,
-                    price: product?.data?.price,
-                    countInStock: product?.data?.countInStock,
-                    quantity: currentQuantity,
-                  })
-                }
+              <Carousel
+                showStatus={false}
+                showArrows={false}
+                showIndicators={false}
+                emulateTouch
+                infiniteLoop
+                autoPlay={true}
+                showThumbs={false}
+                interval={5000}
               >
-                <button>BUY</button>
-              </div>
-            </div>
+                {images.map((item, index) => (
+                  <img key={index} src={item} alt={item} />
+                ))}
+              </Carousel>
+
+              <Carousel
+                id="carousel"
+                showStatus={false}
+                showArrows={false}
+                showIndicators={false}
+                emulateTouch
+                infiniteLoop
+                autoPlay={true}
+                showThumbs={false}
+              >
+                {product?.data?.images?.map((item, index) => (
+                  <img key={index} src={item} alt={item} />
+                ))}
+              </Carousel>
+            </>
           </div>
-          <div className="productDetailsScreen-footer">
-            <div
-              onClick={() => changeFooterBodyActiveState()}
-              className="productDetailsScreen-footer-header"
+          <div className="productDetailsScreen-body-carousel">
+            <Carousel
+              id="carousel"
+              showStatus={false}
+              showArrows={true}
+              // showIndicators={false}
+              emulateTouch
+              // infiniteLoop
+              autoPlay={false}
+              // showThumbs={false}
+              thumbWidth={100}
             >
-              <ArrowDropDownIcon />
-              <span>SPECIFICATIONS</span>
-              <ArrowDropDownIcon />
+              {product?.data?.images?.map((item, index) => (
+                <img key={index} src={item} alt={item} />
+              ))}
+            </Carousel>
+          </div>
+          <div className="productDetailsScreen-body-basic-info">
+            <div className="price">${product?.data?.price}</div>
+            <div>
+              <h3>Characteristics</h3>
+              <ul>
+                <li>
+                  <CheckSharpIcon className="icon" />{" "}
+                  <span>{product?.data?.processorModel}</span>
+                </li>
+                <li>
+                  <CheckSharpIcon className="icon" />{" "}
+                  <span>{product?.data?.memory}</span>
+                </li>
+                <li>
+                  <CheckSharpIcon className="icon" />{" "}
+                  <span>{product?.data?.video}</span>
+                </li>
+                <li>
+                  <CheckSharpIcon className="icon" />{" "}
+                  <span>{product?.data?.storage}</span>
+                </li>
+              </ul>
+            </div>
+
+            <div className="available">
+              {product?.data?.countInStock === 0 ? (
+                ""
+              ) : (
+                <>
+                  <CheckCircleIcon />
+                  <span>Available</span>
+                </>
+              )}
+            </div>
+            <div className="qunatity">
+              {/* <RemoveIcon onClick={() => currentQuantityMinusHandle()} /> */}
+              <input
+                type="number"
+                min="1"
+                defaultValue="1"
+                onChange={(e) => setCurrentQuantity(e.target.value)}
+              />
+              {/* <AddIcon onClick={() => currentQuantityPlusHandle()} /> */}
             </div>
             <div
-              className={`productDetailsScreen-footer-body ${
-                footerBodyActive ? "f-b-active" : ""
-              }`}
+              className="buyBtn"
+              onClick={() =>
+                addToCartHandle({
+                  _id: product.data?._id,
+                  name: product?.data?.name,
+                  image: product?.data?.images[0],
+                  processorModel: product?.data?.processorModel,
+                  memory: product?.data?.memory,
+                  video: product?.data?.video,
+                  storage: product?.data?.storage,
+                  price: product?.data?.price,
+                  countInStock: product?.data?.countInStock,
+                  quantity: currentQuantity,
+                })
+              }
             >
-              <div className="footer-body-row">
-                <div className="property-key">Name</div>
-                <div className="property-value">{product?.data?.name}</div>
-              </div>
-              <div className="footer-body-row">
-                <div className="property-key">Brand</div>
-                <div className="property-value">{product?.data?.brand}</div>
-              </div>
-              <div className="footer-body-row">
-                <div className="property-key">Processor</div>
-                <div className="property-value">{product?.data?.processor}</div>
-              </div>
-              <div className="footer-body-row">
-                <div className="property-key">Processor-model</div>
-                <div className="property-value">
-                  {product?.data?.processorModel}
-                </div>
-              </div>
-              <div className="footer-body-row">
-                <div className="property-key">RAM</div>
-                <div className="property-value">{product?.data?.memory}</div>
-              </div>
-              <div className="footer-body-row">
-                <div className="property-key">Video Card</div>
-                <div className="property-value">{product?.data?.video}</div>
-              </div>
-              <div className="footer-body-row">
-                <div className="property-key">STORAGE</div>
-                <div className="property-value">{product?.data?.storage}</div>
-              </div>
+              <button>BUY</button>
             </div>
           </div>
         </div>
-      )}
+        <div className="productDetailsScreen-footer">
+          <div
+            onClick={() => changeFooterBodyActiveState()}
+            className="productDetailsScreen-footer-header"
+          >
+            <ArrowDropDownIcon />
+            <span>SPECIFICATIONS</span>
+            <ArrowDropDownIcon />
+          </div>
+          <div
+            className={`productDetailsScreen-footer-body ${
+              footerBodyActive ? "f-b-active" : ""
+            }`}
+          >
+            <div className="footer-body-row">
+              <div className="property-key">Name</div>
+              <div className="property-value">{product?.data?.name}</div>
+            </div>
+            <div className="footer-body-row">
+              <div className="property-key">Brand</div>
+              <div className="property-value">{product?.data?.brand}</div>
+            </div>
+            <div className="footer-body-row">
+              <div className="property-key">Processor</div>
+              <div className="property-value">{product?.data?.processor}</div>
+            </div>
+            <div className="footer-body-row">
+              <div className="property-key">Processor-model</div>
+              <div className="property-value">
+                {product?.data?.processorModel}
+              </div>
+            </div>
+            <div className="footer-body-row">
+              <div className="property-key">RAM</div>
+              <div className="property-value">{product?.data?.memory}</div>
+            </div>
+            <div className="footer-body-row">
+              <div className="property-key">Video Card</div>
+              <div className="property-value">{product?.data?.video}</div>
+            </div>
+            <div className="footer-body-row">
+              <div className="property-key">STORAGE</div>
+              <div className="property-value">{product?.data?.storage}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* )} */}
     </div>
   );
 }
